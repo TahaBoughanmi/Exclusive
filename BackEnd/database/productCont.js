d1bf5e16441c8080653b175d25fd4fb5d44fa3d2
unknown <boughanmimohammedtaha@gmail.com>
2024-05-30 11:24:00 +0100

    T

--- BackEnd/database/productCont.js
+++ BackEnd/database/productCont.js
@@ -1,61 +1,90 @@
-const db = require('./config')
+const db = require("./config");
+
 
 module.exports = {
-  getProducts: (req, res) => {
-    db.Product.findAll()
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.send(err);
+  getTopStockProducts: async (req, res) => {
+    try {
+    
+      const topProducts = await db.Product.findAll({
+        limit: 4, 
+        order: [["stock", "DESC"]] 
       });
+      res.json(topProducts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error fetching top products by stock");
+    }
   },
-  addProduct: (req, res) => {
-    db.Product.create(req.body)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  },
-  getOneProduct: (req, res) => {
-    db.Product.findOne({
-      where: {
-        id: req.params.id,
+    getAllproducts: async (req,res)=> {
+      try {
+        const products = await db.Product.findAll();
+        res.json(products);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching products");
+      }
+    },
+      
+      getOneProduct: async (req, res) => {
+        const productId = req.params.productId;
+        console.log(productId);
+        try {
+          const product = await db.Product.findByPk(productId); 
+          if (!product) {
+            return res.status(404).send("product not found");
+          }
+          res.json(product);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error fetching product");
+        }
       },
-    })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  },
-  remove: (req, res) => {
-    db.Product.destroy({
-      where: {
-        id: req.params.id,
+      
+      addProduct: async(req,res) =>{
+        const newProduct = req.body;
+        try {
+          const product = await db.Product.create(newProduct); 
+          res
+            .status(201)
+            .send({ message: "product created successfully", productid: product.id });
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error adding product");
+        }
       },
-    })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  },
-  update: (req, res) => {
-    db.Product.update(req.body, {
-      where: {
-        id: req.params.id,
+      modifyProduct: async (req, res) => {
+        const productId = req.params.productid;
+        const updatedProduct = req.body;
+        try {
+          const product = await db.Product.findByPk(productId);
+          if (!product) {
+            return res.status(404).send("Product not found");
+          }
+          await product.update(updatedProduct);
+          res.status(200).send("Product modified successfully");
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error modifying product");
+        }
       },
-    })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  },
-};
+      removeProduct: async (req, res) => {
+        const productId = req.params.productid;
+        try {
+        await db.Product.destroy({ where:{id:productId}  }); 
+          res.status(201).send('deleted')
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error fetching product");
+        }
+      },
+      getByCategory: async (req, res) => {
+        const category = req.params.category;
+        try {
+        const response=await db.Product.findAll({ where:{category:category}  }); 
+        res.json(response);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error fetching product");
+        }
+      }
+    };
